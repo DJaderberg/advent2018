1087664f2ce566cbe40a432dfa018987654fbaca
David Jäderberg <david@jaderberg.net>
2018-12-26 21:51:40 +0100

    Day 11 part 2 unoptimized. This is way too slow to be used.

--- Advent2018/Day11.fs
+++ Advent2018/Day11.fs
@@ -1,6 +1,7 @@
 namespace Tests
 
 open System
+open System.Linq
 open NUnit.Framework
 
 module Day11 =
@@ -11,7 +12,7 @@ module Day11 =
         then int (chars.[chars.Length-3].ToString())
         else 0
     
-    let powerLevel serial y x =
+    let powerLevel serial x y =
         let rackId = x + 10
         let str =
             rackId
@@ -30,45 +31,62 @@ module Day11 =
         let levelY y = pl y |> List.map (fun (x, level) -> ((x,y),level))
         List.map levelY yrange |> List.concat |> Map.ofList
     
-    let totalPower (powerLevels : Map<(int * int),int>) (x,y) =
-        let tups = [(x,y);(x+1,y);(x+2,y);(x,y+1);(x+1,y+1);(x+2,y+1);(x,y+2);(x+1,y+2);(x+2,y+2)]
-        if x > 298 || y > 298
+    let totalPower (powerLevels : Map<(int * int),int>) size (x,y) =
+        let max = 301 - size
+        let tups = Seq.map (fun z -> Seq.zip (List.replicate size z) (Enumerable.Range(x, size))) (Enumerable.Range(y,size)) |> Seq.concat
+        if x > max || y > max
         then Int32.MinValue
         else Seq.map (fun v -> powerLevels.Item(v)) tups |> Seq.sum
         
-    let largestPower powerLevels =
-        let zoneSum = totalPower powerLevels
+    let largestPower powerLevels size =
+        let zoneSum = totalPower powerLevels size
+        let max = 301 - size
         let tups =
-            List.map (fun x -> List.zip (List.replicate 298 x) [1..298]) [1..298]
+            List.map (fun x -> List.zip (List.replicate max x) [1..max]) [1..max]
             |> List.concat
             |> List.map (fun tup -> (zoneSum tup, tup))
         List.maxBy fst tups
         
+    let largestPowerWithSize powerLevels =
+        let bestPerSize = [1..300] |> List.map (fun s -> (s, largestPower powerLevels s))
+        let conv (size, (power, (x, y))) = (power, x, y, size)
+        let readable = List.map conv bestPerSize
+        let best = List.maxBy (fun (p, x, y, s) -> x) readable
+        let (_,x,y,s) = best
+        (x,y,s)
+        
 
 module Day11Test =
+    //[<Test>]
+    //let Part2() =
+    //    let serial = 8772
+    //    let powerLevels = Day11.powerLevels serial
+    //    let res = snd (Day11.largestPower powerLevels 3)
+    //    Assert.AreEqual((235,31), res)
+        
     [<Test>]
     let Part1() =
         let serial = 8772
         let powerLevels = Day11.powerLevels serial
-        let res = snd (Day11.largestPower powerLevels)
-        Assert.AreEqual((235,31), res)
+        let res = Day11.largestPowerWithSize powerLevels
+        Assert.AreEqual((235,31, 3), res)
 
     [<Test>]
     let PowerLevel0() =
-        let level = Day11.powerLevel 8 5 3
+        let level = Day11.powerLevel 8 3 5
         Assert.AreEqual(4, level)
         
     [<Test>]
     let PowerLevel1() =
-        let level = Day11.powerLevel 57 79 122 
+        let level = Day11.powerLevel 57 122 79
         Assert.AreEqual(-5, level)
 
     [<Test>]
     let PowerLevel2() =
-        let level = Day11.powerLevel 39 196 217 
+        let level = Day11.powerLevel 39 217 196 
         Assert.AreEqual(0, level)
         
     [<Test>]
     let PowerLevel3() =
-        let level = Day11.powerLevel 71 153 101 
+        let level = Day11.powerLevel 71 101 153
         Assert.AreEqual(4, level)
