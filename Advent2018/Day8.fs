85eae402ccb64864110faf06f3b8f6e393fe9ba2
David Jäderberg <david@jaderberg.net>
2018-12-26 11:42:19 +0100

    Day 8 part 2. I don't really understand how the tree construction is working, should be possible to simplify it a lot.

--- Advent2018/Day8.fs
+++ Advent2018/Day8.fs
@@ -9,24 +9,39 @@ type Tree = Node of Tree list * int list
 module Day8 =
     let parseTree (text : string) =
         let ints = text.Split [|' '|] |> Array.map int |> Array.toList
-        let rec readNodes ((tree : Tree list), list) _ =
+        let rec readNodes (list : int list) : (Tree * int list) =
             match list with 
+            | 0 :: numMeta :: xs -> (Node ([], List.take numMeta xs), List.skip numMeta xs)
             | numNodes :: numMeta :: xs -> 
-                let (nodes, remXs) = 
-                    List.replicate numNodes ()
-                    |> List.fold readNodes (tree, xs)
-                let meta = List.take numMeta remXs
-                ([Node (nodes, meta)], List.skip numMeta remXs)
-            | [] -> ([], [])
-            | _ -> raise (new Exception("Parse error!"))
-        let (parsed, []) = readNodes ([], ints) ()
-        List.head parsed
+                let (tree, rem) = readNodes xs
+                let (nextTree, nextRem) = readNodes ((numNodes - 1) :: numMeta :: rem)
+                let (Node (subTree, md)) = nextTree
+                (Node (tree :: subTree, md), nextRem)
+                //(List.concat [tree; nextTree], nextRem) //Puts all on top level
+            | xs -> ((Node ([], [])), xs)
+        let (parsed, _) = readNodes ints
+        parsed
         
         
     let rec sumMetadata (tree : Tree) = 
         match tree with 
         | Node (treeList, meta) -> List.sum (List.map sumMetadata treeList) + (List.sum meta)
-            
+        
+    let rec value (tree : Tree) =
+        match tree with 
+        | Node ([], meta) -> List.sum meta
+        | Node (children, refs) -> 
+            let valueOfChild ref = 
+                let child : Tree option =
+                    if children.Length >= ref
+                    then 
+                        Some (children.[ref - 1])
+                    else 
+                        None
+                match child with 
+                | Some ct -> value ct
+                | None -> 0
+            List.sum (List.map valueOfChild refs)
         
 
 [<TestClass>]
@@ -36,13 +51,28 @@ type Day8 () =
         let file = "../../../../../../code/advent/input8.txt"
         let text = System.IO.File.ReadAllText(file)
         let trees = Day8.parseTree text
-        let sum = Day8.sumMetadata trees
+        let sum = Day8.sumMetadata (trees)
         Assert.AreEqual(36027, sum)
         
     [<Test>]
     member this.Part1example() =
         let text = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
         let trees = Day8.parseTree text
-        let sum = Day8.sumMetadata trees
+        let sum = Day8.sumMetadata (trees)
         Assert.AreEqual(138, sum)
+        
+    [<Test>]
+    member this.Part2() =
+        let file = "../../../../../../code/advent/input8.txt"
+        let text = System.IO.File.ReadAllText(file)
+        let trees = Day8.parseTree text
+        let value = Day8.value (trees)
+        Assert.AreEqual(23960, value)
+        
+    [<Test>]
+    member this.Part2example() =
+        let text = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+        let trees = Day8.parseTree text
+        let value = Day8.value (trees)
+        Assert.AreEqual(66, value)
 
